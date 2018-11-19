2e7985378de65e9af5c7a539828c4e7e259d0885
WHQ <2283149823@qq.com>
2018-11-19 21:11:45 +0800

    update

--- NewGrowth/src/test/java/com/org/growth/GrowthApplicationTests.java
+++ NewGrowth/src/test/java/com/org/growth/GrowthApplicationTests.java
@@ -1,4 +1,4 @@
-package com.org.Backend;
+package com.org.growth;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,10 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class BackendApplicationTests {
+public class GrowthApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
